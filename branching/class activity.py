2d2e219a4df9f21826c9f864064cd90866de3f44
ABC <a@b.com>
2026-04-12 10:14:24 +0530

    Branching in python

--- branching/class activity.py
+++ branching/class activity.py
@@ -1,3 +1,4 @@
+# 1. Speed Tracker
 # You are driving a car
 # Speed sensors track the speed
 
@@ -11,8 +12,8 @@
 # print the message accordingly
 
 
-speed = float(input("Enter the speed (km/hr): "))
-
+#speed = float(input("Enter the speed (km/hr): "))
+speed =30
 if speed < 30:
     print("Very Slow")
 elif speed >= 30 and speed < 50:
@@ -21,3 +22,57 @@ elif speed >= 50 and speed < 80:
     print("Fast but under the limits")
 else:
     print("Fined")
+
+
+
+# 2. Design a Login System
+
+#Ask for:
+#    username
+#    password
+# Check against predefined values and print:
+
+# O/P :
+#   "Login successful"
+#   "Invalid credentials"
+
+
+
+# 3. Voting Eligibility with Conditions
+
+# Input:
+#   age
+#   citizenship (yes/no)
+
+# Output:
+#   Print if the person is eligible to vote.
+
+
+
+
+# 4. Simple Calculator (No loops)
+
+# Input:
+#   two numbers
+#   one operator (+, -, *, /)
+
+# Output : Value after applying the operator
+# Use branching to perform the operation.
+
+
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+op = input("Enter the operator (+, -, *, /) :")
+
+#print(num1, op, num2)
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
